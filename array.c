1ec5087ac9e4a3af5f569697a90890c4808b709b
pazi9989 <141890394+pazi9989@users.noreply.github.com>
2023-08-18 09:58:18 +1000

    Update array.c

--- array.c
+++ array.c
@@ -1,1 +1,47 @@
 
+#include "array.h"
+list_t createList() {
+	return NULL;
+}
+
+// insert at start
+list_t insert(data_t *d, list_t l) {
+	node_t *node= my_malloc(sizeof(*node));
+	node->d= d;
+	node->next=l;
+	return node;
+}
+
+void search(char *key, list_t l, FILE *f) {
+	int n=0;
+	node_t *p= l;
+	// printf("search for [%s]\n",key);
+	while (p != NULL) {
+		if (strcmp(p->d->t_name,key)==0) {
+			n++;
+			fprintf(f, "%s --> ", key);
+			printData(f,p->d);
+		}
+        	p= p->next;
+	}
+	if (n==0) fprintf(f, "%s --> NOT FOUND\n", key);
+}
+
+
+void free_list(list_t l){
+	node_t *p= l;
+	while (p != NULL) {
+		node_t *t= p;
+		p= p->next;
+		freeData(t->d);
+		free(t);
+	}
+}
+void *my_malloc(size_t n){
+	assert(n>0);
+	void *p= malloc(n);
+	assert(p);
+	return p;
+} 
+
+
