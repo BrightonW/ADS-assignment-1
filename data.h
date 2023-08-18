1fa4111da6e2e8eccac7bc622e894ad43363fc70
pazi9989 <141890394+pazi9989@users.noreply.github.com>
2023-08-18 16:20:45 +1000

    Update data.h

--- data.h
+++ data.h
@@ -1,48 +1,30 @@
 #ifndef _DATA_H_
 #define _DATA_H_
 
-//#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
 
-#define C ','     
-#define Q '\"'   
 #define NL '\n' 
+#define FIELDS 14
 
-#define MAX_LEN 128
-
-// Note: it's more convenient to typedef data_t as char**
-//       but the requirement is to use struct ;-(
-typedef struct {
-	char *cyear, *block_id, *prop_id, *base_id;  // 0-3
-	char *build_add, *c_s_a; *biz_add;   // 4, 5
-	char *t_name; *code;                     // 6
-	char *desc;                     // 7
-	char *x, *y;                    // 8,9
-	char *loc;                      // 10;
-} data_t;
-
-typedef struct {
-    int cyear;
-    int block_id;
-    int prop_id;
-    int base_id;
-    char build_add[256];
-    char c_s_a[256];
-    char biz_add[256];
-    char t_name[256];
-    int i_code;
-    char i_desc[256];
-    char seat_type[256];
-    int num_seats;
-    double longitude;
-    double latitude;
-}data_t;
-
-void print_data(data_t *d, FILE *f);  // print a data to file f
-void free_data(data_t *d);            // 
-data_t *get_next_data(FILE *f);       // return a next record read from f
-
-void data_get_header(FILE *f);       // get rid off & and store header for late use
-void data_free_header();             // free the sored header
-
-char *getfield(FILE *f, char deli);  // get a next field from file f
+#define FIELD_LEN 128
+struct data {
+	char *cyear,*block_id,*prop_id;
+	char *base_id;
+	char *build_add, *c_s_a, *biz_add;
+	char *t_name;
+	char *i_code,*i_desc;
+	char *seat_type,*num_seats;
+	char *longi,*lat;
+};
+typedef struct data data_t;
+void dataPrintHeader(FILE *file);       // get rid off & and store header for late use
+void printData(FILE *file, data_t *data);
+void freeHeader();
+char *read_field(FILE *f, char deli);
+data_t *get_next_data(FILE *f);
+void freeData(data_t *data);
 #endif
